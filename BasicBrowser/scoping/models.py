4830af1e0e7e8017748a8d2003c29b3192ac71fc
Max <max.w.callaghan@gmail.com>
2017-02-08 19:03:53 +0100

    some progress on adding scopus docs to db

--- BasicBrowser/scoping/models.py
+++ BasicBrowser/scoping/models.py
@@ -17,6 +17,7 @@ class Query(models.Model):
     type     = models.TextField(null=True, verbose_name="Query Type")
     title    = models.TextField(null=True, verbose_name="Query Title")
     text     = models.TextField(null=True, verbose_name="Query Text")
+    database = models.CharField(max_length=6,null=True, verbose_name="Query database")
     date     = models.DateTimeField(verbose_name="Query Date")
     r_count  = models.IntegerField(null=True, verbose_name="Query Results Count")
     creator  = models.ForeignKey(User,null=True, verbose_name="Query Creator", related_name="user_creations")
