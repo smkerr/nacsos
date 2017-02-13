0ea15cfd767d5f858610fcd1506db56092e27f13
jhilaire <hilaire@mcc-berlin.net>
2017-02-13 18:21:45 +0100

    Snowballing update

--- BasicBrowser/scoping/admin.py
+++ BasicBrowser/scoping/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+admin.site.register(Technology)
 admin.site.register(SnowballingSession)
 admin.site.register(Query)
 admin.site.register(Doc)
 admin.site.register(WoSArticle)
+admin.site.register(DocOwnership)
+admin.site.register(DocAuthInst)
+admin.site.register(DocReferences)
+admin.site.register(Tag)
+admin.site.register(Note)
