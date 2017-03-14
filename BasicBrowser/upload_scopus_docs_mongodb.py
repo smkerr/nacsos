7fbe0976571abaf05bde49c877c5254828c18f81
mcallaghan <max.w.callaghan@gmail.com>
2017-03-14 11:43:45 +0100

    update documentation

--- BasicBrowser/upload_scopus_docs_mongodb.py
+++ BasicBrowser/upload_scopus_docs_mongodb.py
@@ -176,7 +176,10 @@ def main():
     if chunk_size < max_chunk_size:
         # parallely add docs
         pool = Pool(processes=6)
-        pool.map(add_doc_text, records)
+        if very_par:
+            pool.map(add_doc_text, records)
+        else:
+            pool.map(add_doc, records)
         pool.terminate()
         #result = db.scopus_docs.insert_many(records)
 
