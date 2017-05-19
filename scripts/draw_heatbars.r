830ab4b50319639ab63062f56979d98442498518
mcallaghan <max.w.callaghan@gmail.com>
2017-05-19 11:56:07 +0200

    heatbar graphs

---
+++ scripts/draw_heatbars.r
@@ -0,0 +1,48 @@
+rm(list=ls())
+#==== USER SECTION ==========
+u_sheetName <- "BECCS (so far only bioenergy potential)"
+#u_sheetName <- "Afforestation and Reforestation"
+#u_sheetName <- "DAC"
+u_sheetName <- "BECCS (Bioenergy)"
+DEBUG       <- TRUE
+
+#==== INITIALISE ==========
+# Load libraries
+library(googlesheets)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+source("heatbar_functions.R")
+
+# Authorise googlesheets to access your Google Sheets account
+gs_auth()
+
+
+#==== READ IN SPREADSHEET ==========
+gs  <- gs_title("NETs Review")
+ss  <- gs_read(gs, ws = u_sheetName,verbose=DEBUG)
+
+data <- get_data(ss)
+
+################################################
+## Generate a new df of ranges
+
+ranges <- seq(1,1000)
+df <- data.frame(v=ranges)
+
+# Get a list of resources, or define it yourself
+resources <- unique(
+  data[data$measurement=="max" & data$variable!="cost",]$variable
+)
+resources <- resources[!is.na(resources)]
+
+# Count the studies with a maximum under each range for each resource
+
+res2050 <- countranges(df,filter(data,year=="2050"),resources)
+
+heatbar(res2050,"pcnt") +
+  labs(x="Resource",y="Potential [EJ/yr]")
+
+
+
---
+++ scripts/heatbar_functions.R
@@ -0,0 +1,107 @@
+get_data <- function(ss){
+  if (dim(ss)[1] <= 2) stop("No data in selected spreadsheet")
+  
+  #==== PROCESS DATA ==========
+  # Get 3 header lines
+  header1 <- names(ss)
+  header2 <- paste(ss[1,])
+  header3 <- paste(ss[2,])
+  
+  # Make a new data frame with the actual data
+  data <- ss[3:dim(ss)[1], ]
+  
+  # loop through and combine the column names
+  lcol1 <- ''
+  lcol2 <- ''
+  lcol3 <- ''
+  separator <- 'Â§'
+  for (col in seq(1,dim(ss)[2])) {
+    h1 <- if (grepl('^X[0-9]+$',header1[col])) lcol1 else header1[col]
+    lcol1 <- h1
+    h2 <- if (grepl('^NA$',header2[col])) lcol2 else header2[col]
+    lcol2 <- h2
+    h3 <- if (grepl('^NA$',header3[col])) lcol3 else header3[col]
+    lcol3 <- h3
+    var <- paste(h1,h2,h3,sep=separator)
+    names(data)[col] <- var
+  } 
+  
+  # Switch the data to long format
+  data <- data %>% 
+    gather_("variable", "value",names(data)[7:dim(ss)[2]]) %>% 
+    separate(variable, into=c("category", "subcategory", "variable"), sep="Â§") %>%
+    separate(
+      variable,into=c("variable","measurement"), 
+      sep="\\.",fill="right",extra="drop"
+    )
+  
+  
+  names(data) <- gsub(separator,'',names(data))
+  names(data) <- gsub('Dimensions','',names(data))
+  
+  return(data)
+}
+
+countranges <- function(df,data,headers) {
+  
+  countrange <- function(x,resource) {
+    data <- filter(
+      suppressWarnings(mutate(data,value=as.numeric(value))),
+      measurement=="max",
+      variable==resource,
+      !is.na(value),
+      value>=x
+    )
+    if (length(data$TI) > length(unique(data$TI))) {
+      print("warning, some titles seem to be duplicated, do you
+            need to filter by a dimension?")
+    } 
+    return(as.numeric(count(data)))
+    }
+  
+  
+  
+  # For each resource, count the number of values under each threshold
+  for (r in resources) {
+    df[[r]] <- as.numeric(lapply(df$v,countrange,r))
+  }
+  
+  # Gather the resources
+  df <- df %>%
+    gather(resource,value,-v) %>% #gather resources into a resource column
+    group_by(resource) %>% # group by resource
+    mutate(maxvalue = max(value)) %>% # calculate the maximum (total number of studies) in each resource
+    group_by(value) %>% # group by each value
+    mutate(pcnt=value/maxvalue*100) # calculate the value as a pcnt of the total
+}
+
+heatbar <- function(df,f) {
+  flab <- if (f=="pcnt") "% of Studies" else "Number of Studies" 
+  df <- df[df[[f]]>0,]
+  df <- df %>%
+    group_by(resource) %>%
+    mutate(resourcelab=paste0(resource,'\n[',max(value),' studies]'))
+  ggplot() +
+    theme_bw() +
+    geom_bar(
+      data=df,
+      aes_string(x="resourcelab",y=1,fill=f),
+      stat="identity",
+      width=0.6,
+      color=NA
+    ) +
+    geom_bar(
+      data=df,
+      aes(x=resourcelab),
+      fill=NA,
+      color="grey22",
+      width=0.6
+    ) +
+    scale_fill_gradientn(
+      colours=c(NA,"#fffcef","#fcf0ba","#d30000"),
+      values = scales::rescale(c(0,min(df[[f]]),max(df[[f]])/2,max(df[[f]]))),
+      name=flab
+    ) +
+    guides(fill = guide_colourbar(reverse = TRUE))
+}
+
