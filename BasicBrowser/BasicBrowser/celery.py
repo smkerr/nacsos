f08e88078c3c545333b66888dce6cc3739b28db8
Max <max.w.callaghan@gmail.com>
2017-07-27 17:33:13 +0200

    celery setup and working

---
+++ BasicBrowser/BasicBrowser/celery.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BasicBrowser.settings')
+
+app = Celery(
+    'BasicBrowser',
+    broker='redis://localhost:6379/0'
+)
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
--- BasicBrowser/BasicBrowser/settings.py
+++ BasicBrowser/BasicBrowser/settings.py
@@ -142,3 +142,9 @@ CACHES = {
         }
     }
 }
+
+## CELERY SETTINGS
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
