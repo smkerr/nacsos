80ef61c67b9a2f7e83bd3269eae7a399622ccacc
Jérôme Hilaire <hilj@mcc-berlin.net>
2018-06-06 13:18:05 +0200

    Bugfix views.py

--- BasicBrowser/scoping/views.py
+++ BasicBrowser/scoping/views.py
@@ -3930,9 +3930,11 @@ def highlight_words_new(s,query):
         print("At least one match was found")
         match_found = True
         text_highlighted.append(s[0:(m.start()-1)]+'<span class="t1">'+s[m.start():m.end()]+'</span>')
-        kpos <- m.end()+1
+        kpos = m.end()+1
         # Loop over potential other matches
+        iter = 1
         while kpos <= nchar and match_found:
+            print(iter)
             match_found = False 
             m = pattern.search(s, kpos)
             if m is not None:
