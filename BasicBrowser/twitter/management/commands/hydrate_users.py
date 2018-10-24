90d659af69990e21b33de9d273a15343de637eb7
Max <max.w.callaghan@gmail.com>
2018-10-24 17:59:13 +0200

    upload search hydration

--- BasicBrowser/twitter/management/commands/hydrate_users.py
+++ BasicBrowser/twitter/management/commands/hydrate_users.py
@@ -32,12 +32,36 @@ class Command(BaseCommand):
                         except:
                             print(f)
                             pass
+                user.fetched = timezone.now()
                 user.save()
+                for p in range(0,160):
+                    statuses = api.user_timeline(u.screen_name)
+                    for s in statuses:
+                        status, created = Status.objects.get_or_create(
+                            id=s.id
+                        )
+                        sdata = s._json
+                        for f in sdata:
+                            if sdata[f] != "none":
+                                try:
+                                    if f=="created_at":
+                                        sdata[f] = datetime.strptime(sdata[f],tf)
+                                    field = Status._meta.get_field(f)
+                                    setattr(status, f, sdata[f])
+                                except:
+                                    pass
+                        status.author = user
+                        status.api_got = True
+                        status.fetched = timezone.now()
+                        status.save()
+
+
+
 
         auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
         auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_SECRET)
         api = tweepy.API(auth,wait_on_rate_limit=True)
-        users = User.objects.filter(monitoring=True)
+        users = User.objects.filter(monitoring=True).order_by('fetched')
         ulookup = []
         for u in users:
             ulookup.append(u.id)
