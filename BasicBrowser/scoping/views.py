3487dae145299705bc6ffa7eec5f982d90c57e00
Max <max.w.callaghan@gmail.com>
2018-10-24 15:46:22 +0200

    view fancy tokenize

--- BasicBrowser/scoping/views.py
+++ BasicBrowser/scoping/views.py
@@ -1622,7 +1622,7 @@ def query_tm(request,qid):
         'query': query,
         'form': form,
         'project': query.project,
-        'fields_1': ['min_freq','max_df','max_features','limit','ngram','fulltext','citations'],
+        'fields_1': ['min_freq','max_df','max_features','limit','ngram','fulltext','citations','fancy_tokenization'],
         'fields_2': ['K','alpha','max_iterations','db'],
         'fields_3': ['method']
     }
