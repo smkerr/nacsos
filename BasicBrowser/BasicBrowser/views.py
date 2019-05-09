0fa927c215c5525d998899b0c8b0ddaa4c1a66e5
fmhansen <finnmh@posteo.de>
2019-05-09 15:16:09 +0200

    disable parliament view on base page

--- BasicBrowser/BasicBrowser/views.py
+++ BasicBrowser/BasicBrowser/views.py
@@ -9,8 +9,8 @@ def index(request):
     template = loader.get_template('scoping/base_index.html')
     apps = [
         {'name': 'scoping','app_url': '/scoping'},
-        {'name': 'tmv_app','app_url': '/tmv_app/runs'},
-        {'name': 'parliament', 'app_url': '/parliament'}
+        {'name': 'tmv_app','app_url': '/tmv_app/runs'}
+        # {'name': 'parliament', 'app_url': '/parliament'}
     ]
     context = {
         'site_header': 'MCC-APSIS',
