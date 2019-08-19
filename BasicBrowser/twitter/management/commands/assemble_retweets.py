c92bd5b62545c4b12cd6dc17e9094c438d39c7a4
mcallaghan <max.w.callaghan@gmail.com>
2019-08-19 10:46:42 +0200

    fix abstract edit

--- BasicBrowser/twitter/management/commands/assemble_retweets.py
+++ BasicBrowser/twitter/management/commands/assemble_retweets.py
@@ -12,34 +12,56 @@ from django.utils import timezone
 import time
 from pathlib import Path
 import re
+import random
 
 class Command(BaseCommand):
     help = 'redoes searches'
 
+    def add_arguments(self, parser):
+        parser.add_argument('pid',type=int)
+
     def handle(self, *args, **options):
-        rts = Status.objects.filter(
-            text__icontains="kohle",
-            text__regex="^RT @",
-            retweeted_status__isnull=True
-        )
-        print(rts.count())
-        texts = rts.values_list('text',flat=True).distinct()
-        for t in texts:
-            # Get the statuses which have this same text
-            trts = Status.objects.filter(text=t)
-            # The original text is that which comes after the first colon
-            otext = ":".join(t.split(':')[1:]).strip()
-            ltext = "^"+re.sub('(\S*…)','',otext).strip()
-            try:
-                ostatus = Status.objects.get(text=otext)
-            except:
-                try:
-                    ostatus = Status.objects.get(text__regex=ltext)
-                except:
+
+        pid = pk=options['pid']
+
+        for search in TwitterSearch.objects.filter(project_id=pid):
+
+            print(search.string)
+
+            rts = search.status_set.filter(
+                text__regex="^RT @",
+                retweeted_status__isnull=True
+            )#[:4000]
+            rt_count = rts.count()
+            print(rt_count)
+            texts = list(set(rts.values_list('text',flat=True)))
+            random.shuffle(texts)
+            done_texts = []
+            for i, t in enumerate(texts):
+                if i % 100 == 0:
+                    print(f"{i/rt_count:.2%}")
+                if t in done_texts:
                     continue
-            ostatus.retweeted=True
-            for s in trts:
-                ostatus.retweeted_by.add(s.author)
-                s.retweeted_status = ostatus
-                s.save()
-            ostatus.save()
+                if  ":" not in t:
+                    continue
+                # Get the statuses which have this same text
+
+                trts = search.status_set.filter(text=t)
+
+                # The original text is that which comes after the first colon
+                otext = ":".join(t.split(':')[1:]).strip()
+                ltext = re.sub('(\S*…)','',otext).strip()
+                try:
+                    ostatus = Status.objects.get(text=otext)
+                except:
+                    try:
+                        ostatus = Status.objects.get(text__startswith=ltext)
+                    except:
+                        continue
+                done_texts.append(t)
+                ostatus.retweeted=True
+                for s in trts.iterator():
+                    ostatus.retweeted_by.add(s.author)
+                    s.retweeted_status = ostatus
+                    s.save()
+                ostatus.save()
