bffcc7a163acdd882fbe6c5833f7f4c24a26e451
mcallaghan <max.w.callaghan@gmail.com>
2019-10-24 14:59:48 +0200

    unlimited users

--- BasicBrowser/draw_models.sh
+++ BasicBrowser/draw_models.sh
@@ -1,5 +1,7 @@
 python3 manage.py graph_models scoping | dot -Tpdf > models.pdf
 python3 manage.py graph_models tmv_app | dot -Tpdf > models_tmv.pdf
 python3 manage.py graph_models parliament | dot -Tpdf > models_parliament.pdf
+python3 manage.py graph_models twitter | dot -Tpdf > models_twitter.pdf
+python3 manage.py graph_models twitter parliament | dot -Tpdf > models_twitter_parliament.pdf
 python3 manage.py graph_models parliament | dot -Tpng > models_parliament.png
 python3 manage.py graph_models scoping tmv_app  | dot -Tpdf > all_models.pdf
