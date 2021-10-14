c5f72bbee521886d6fc07b45fe43ba1323fbb565
Max Callaghan <max.w.callaghan@gmail.com>
2021-10-14 11:35:32 +0200

    registration urls

--- BasicBrowser/utils/utils.py
+++ BasicBrowser/utils/utils.py
@@ -566,7 +566,7 @@ def add_scopus_doc(r,q,update, find_ids = True):
             print("more than one doc matching!!!!!")
             wdocs = docs.filter(UT__UT__contains='WOS:')
             if wdocs.count()==1:
-                docs.exclude(UT__UT__contains='WOS:').delete()
+                #docs.exclude(UT__UT__contains='WOS:').delete()
                 doc = wdocs.first()
             else:
                 doc = docs.first()
@@ -589,6 +589,7 @@ def add_scopus_doc(r,q,update, find_ids = True):
                 pass
             except MultipleObjectsReturned:
                 print(f"found multiple similar objects for {get(r,'ti')}")
+                # Handle this case..
 
         # If we still have zero matches, create a new document
         if doc is None:
