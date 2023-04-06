52c772c88e9c009661ec637e73c0d0d4851ffbb7
Tim Repke <repke@mcc-berlin.net>
2023-04-06 19:42:58 +0200

    add urlpattern based on https://docs.djangoproject.com/en/3.0/ref/contrib/admin/#multiple-admin-sites-in-the-same-urlconf

--- BasicBrowser/BasicBrowser/urls.py
+++ BasicBrowser/BasicBrowser/urls.py
@@ -10,7 +10,7 @@ from django.conf import settings
 from django.urls import re_path
 import scoping
 
-#handler404 = scoping.views.handler404
+# handler404 = scoping.views.handler404
 
 
 if settings.MAINTENANCE:
@@ -19,27 +19,33 @@ if settings.MAINTENANCE:
     ]
 else:
     urlpatterns = [
-        #path('lotto/', include('lotto.urls')),
-        path('admin/doc/', include('django.contrib.admindocs.urls')),
-        #re_path(r'^', scoping.views.handler404, name="maintenance"),
-        #path('404', page_not_found, {'exception': Exception()}),
-        re_path(r'^tmv_app/', include('tmv_app.urls')),
-        re_path(r'^scoping/', include('scoping.urls')),
-        re_path(r'^parliament/', include('parliament.urls')),
-        path('twitter/', include('twitter.urls')),
-        path('lotto', include('lotto.urls')),
-        path('signup', site_views.signup, name='signup'),
-        re_path(r'^admin/', admin.site.urls),
-        re_path(r'^$', site_views.index),
-        #path('accounts/', include('django.contrib.auth.urls')),
-        path('accounts/password_reset', auth_views.PasswordResetView.as_view(success_url="/"), name="password_reset"),
-        path('accounts/password_change', auth_views.PasswordChangeView.as_view(success_url="/scoping"), name="password_change"),
-        path('accounts/login/', auth_views.LoginView.as_view(success_url="/"), {'template_name': 'scoping/login.html'}),
-        re_path(r'^accounts/logout/$', views.logout_view, name='logout_view'),
-]
+        path('nacsos-legacy/', include([
+            # path('lotto/', include('lotto.urls')),
+            path('admin/doc/', include('django.contrib.admindocs.urls')),
+            # re_path(r'^', scoping.views.handler404, name="maintenance"),
+            # path('404', page_not_found, {'exception': Exception()}),
+            re_path(r'^tmv_app/', include('tmv_app.urls')),
+            re_path(r'^scoping/', include('scoping.urls')),
+            re_path(r'^parliament/', include('parliament.urls')),
+            path('twitter/', include('twitter.urls')),
+            path('lotto', include('lotto.urls')),
+            path('signup', site_views.signup, name='signup'),
+            re_path(r'^admin/', admin.site.urls),
+            re_path(r'^$', site_views.index),
+            # path('accounts/', include('django.contrib.auth.urls')),
+            path('accounts/password_reset', auth_views.PasswordResetView.as_view(success_url="/"),
+                 name="password_reset"),
+            path('accounts/password_change', auth_views.PasswordChangeView.as_view(success_url="/scoping"),
+                 name="password_change"),
+            path('accounts/login/', auth_views.LoginView.as_view(success_url="/"),
+                 {'template_name': 'scoping/login.html'}),
+            re_path(r'^accounts/logout/$', views.logout_view, name='logout_view'),
+        ]))
+    ]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += [
         re_path(r'^__debug__/', include(debug_toolbar.urls)),
     ]
